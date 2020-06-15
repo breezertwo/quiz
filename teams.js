90dbadd47fa6610d31b800233c5752c8b79d1393
breezertwo <brechtelandreas@gmail.com>
2020-06-15 16:52:36 +0200

    prepare quiz tonight

--- teams.js
+++ teams.js
@@ -1,14 +1,15 @@
 module.exports = [
 	{ id: 1, photo: '', name: 'Fex', members: [], score: 0, connections: 0 },
-	{ id: 2, photo: '', name: 'Schmi3dy', members: [], score: 0, connections: 0 },
+	{ id: 2, photo: '', name: 'Andy', members: [], score: 0, connections: 0 },
 	{ id: 3, photo: '', name: 'Chrissy', members: [], score: 0, connections: 0 },
 	{ id: 4, photo: '', name: 'Ka', members: [], score: 0, connections: 0 },
 	{ id: 5, photo: '', name: 'Franzi', members: [], score: 0, connections: 0 },
-	{ id: 6, photo: '', name: 'Hörby', members: [], score: 0, connections: 0 },
+	{ id: 6, photo: '', name: 'Tom', members: [], score: 0, connections: 0 },
 	{ id: 7, photo: '', name: 'Franca', members: [], score: 0, connections: 0 },
 	{ id: 8, photo: '', name: 'Chris', members: [], score: 0, connections: 0 },
 	{ id: 9, photo: '', name: 'Dubbi', members: [], score: 0, connections: 0 },
 	{ id: 10, photo: '', name: 'Laurin', members: [], score: 0, connections: 0 },
 	{ id: 11, photo: '', name: 'Simon', members: [], score: 0, connections: 0 },
-	{ id: 12, photo: '', name: 'Lena', members: [], score: 0, connections: 0 }
+	{ id: 12, photo: '', name: 'Lena', members: [], score: 0, connections: 0 },
+	{ id: 13, photo: '', name: 'Lukas', members: [], score: 0, connections: 0 },
 ]
